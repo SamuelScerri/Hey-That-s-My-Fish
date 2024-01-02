5232a94e5aeb90f405ef6162935c1665395f4fc0
Samuel Scerri <samuelscerri533@gmail.com>
2024-01-02 18:16:54 +0100

    Added Synchronized Board Generation

---
+++ Assets/Scripts/TileView.cs
@@ -0,0 +1,50 @@
+using Photon.Pun;
+using UnityEngine;
+
+
+
+public class TileView : MonoBehaviour, IPunObservable
+{
+	[SerializeField]
+	private byte amount;
+	private SpriteRenderer spriteRenderer;
+
+	public enum State
+	{
+		Empty,
+		Occupied,
+		Full,
+	}
+
+	public byte Amount
+	{
+		get => amount;
+		set
+		{
+			amount = value;
+			spriteRenderer.sprite = BoardManager.Board.TileSprites[value];
+		}
+	}
+
+	public State CurrentState { get; set; }
+
+	private void Awake()
+	{
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.IsWriting)
+		{
+			stream.SendNext(Amount);
+			stream.SendNext(CurrentState);
+		}
+			
+		else if (stream.IsReading)
+		{
+			Amount = (byte)stream.ReceiveNext();
+			CurrentState = (State)stream.ReceiveNext();
+		}
+	}
+}
