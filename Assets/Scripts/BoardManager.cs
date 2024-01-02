7da4c56607a72fec29f5b6e52cd6acb077fce6cc
Samuel Scerri <samuelscerri533@gmail.com>
2024-01-02 19:31:48 +0100

    Added Penguin & Updated Board Manager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class BoardManager : MonoBehaviour
+public class BoardManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
+	public const byte ChooseAreaEvent = 1;
+
 	[SerializeField] private Vector2Int boardSize;
 	[SerializeField] private Vector2 tileSize, tileMargin;
 	[SerializeField] private float cameraOffset;
@@ -12,6 +16,8 @@ public class BoardManager : MonoBehaviour
 
 	public static BoardManager Board { get; set; }
 	public Sprite[] TileSprites { get => tileSprites; }
+
+
 
 	private void Awake()
 	{
@@ -23,10 +29,7 @@ public class BoardManager : MonoBehaviour
 
 	private void Start()
 	{
-		Camera.main.transform.position = new Vector3(boardSize.x * (tileSize.x + tileMargin.x) / 2 - cameraOffset, Camera.main.transform.position.y, boardSize.y * (tileSize.y + tileMargin.y) / 2);
-
-		if (PhotonNetwork.IsMasterClient)
-			StartCoroutine(InitializeBoard());
+		Camera.main.transform.position = new Vector3(boardSize.x * (tileSize.x + tileMargin.x) / 2 - cameraOffset, Camera.main.transform.position.y, boardSize.y * (tileSize.y + tileMargin.y) / 2);	
 	}
 
 	private IEnumerator InitializeBoard()
@@ -35,10 +38,45 @@ public class BoardManager : MonoBehaviour
 			for (byte y = (byte)(x % 2 == 0 ? 0 : 1); y < boardSize.y; y++)
 			{
 				TileView tile = PhotonNetwork.Instantiate("Tile", new Vector3(x * (tileSize.x + tileMargin.x), 0, y * (tileSize.y + tileMargin.y) + (x % 2 == 0 ? (tileSize.y + tileMargin.y) / 2 : 0)), Quaternion.identity).GetComponent<TileView>();
-				tile.Amount = (byte)Random.Range(0, 4);
+				tile.Amount = (byte)Random.Range(1, 4);
 				tile.CurrentState = TileView.State.Full;
 			}
-		
+
+		PhotonNetwork.RaiseEvent(ChooseAreaEvent, null, new RaiseEventOptions { Receivers = ReceiverGroup.All }, SendOptions.SendReliable);
+
 		yield return null;
+	}
+
+	private IEnumerator ChooseAreaCoroutine()
+	{
+		byte penguinsLeft = 4;
+
+		while (penguinsLeft > 0)
+		{
+			yield return new WaitForEndOfFrame();
+		}
+
+		yield return null;
+	}
+
+	public void OnEvent(EventData photonEvent)
+	{
+		if (photonEvent.Code == ChooseAreaEvent)
+		{
+			print("Please Choose Tiles");
+			StartCoroutine(ChooseAreaCoroutine());
+		}
+	}
+
+	public override void OnPlayerLeftRoom(Player otherPlayer)
+	{
+		print("Exiting!");
+		Application.Quit();
+	}
+
+	public override void OnPlayerEnteredRoom(Player newPlayer)
+	{
+		if (PhotonNetwork.IsMasterClient)
+			StartCoroutine(InitializeBoard());
 	}
 }
--- Assets/Scripts/TileView.cs
+++ Assets/Scripts/TileView.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class TileView : MonoBehaviour, IPunObservable
 {
-	[SerializeField]
 	private byte amount;
 	private SpriteRenderer spriteRenderer;
 
